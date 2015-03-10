fa4d975b5ca10e1d1ca5c22927ff7fdeefd1d81d
Justyn Harriman <jharriman@uchicago.edu>
2015-03-10 17:34:14 -0500

    Started on browser visualization

--- geometry.py
+++ geometry.py
@@ -38,6 +38,6 @@ if __name__ == "__main__":
     phrase = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".replace(".", "").replace(",", "")
     enc = ArithmeticEncoder("arithmetic/English_word_list.txt")
     sequence = enc.encodePhrase(phrase)
-    createConnections(sequence, 5)
+    createConnections(sequence, 50)
     # print sequence
     # print enc.decodeSet(sequence)
